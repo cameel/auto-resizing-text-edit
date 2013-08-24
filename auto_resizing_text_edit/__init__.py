82a71bc0c70a03ed786f0ff9f1e3e8f65a46d120
cameel <cameel2@gmail.com>
2013-08-24 20:39:32 +0200

    Make things directly importable from auto_resizing_text_edit module

---
+++ auto_resizing_text_edit/__init__.py
@@ -0,0 +1,4 @@
+# This file defines the public API of the library.
+# The things we import here are accessible to the user from auto_resizing_text_edit.
+
+from .auto_resizing_text_edit import AutoResizingTextEdit
